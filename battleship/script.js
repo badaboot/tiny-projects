0473caf5c0c6bbbfc41c574cde4496121a5d92a1
Anny He <annyannyhehe@gmail.com>
2024-11-17 22:24:08 -0800

    fix bug

--- battleship/script.js
+++ battleship/script.js
@@ -13,15 +13,14 @@ grid.forEach((row) => {
 });
 
 const shipSizeMap = {
-  Carrier: { size: 5, num: 1 },
-  Battleship: { size: 4, num: 2 },
-  Destroyer: { size: 3, num: 3 },
-  Submarine: { size: 3, num: 4 },
-  "Patrol Boat": { size: 2, num: 5 },
+  Carrier: { size: 5, num: 1, coords: [] },
+  Battleship: { size: 4, num: 2, coords: [] },
+  Destroyer: { size: 3, num: 3, coords: [] },
+  Submarine: { size: 3, num: 4, coords: [] },
+  "Patrol Boat": { size: 2, num: 5, coords: [] },
 };
 
-// TODO: check coordinates don't overlap with existing boats on map
-const allShipCoords = [];
+const allShipCoords = new Set();
 
 // utils function
 function randomIntFromInterval(min, max) {
@@ -31,15 +30,23 @@ function randomIntFromInterval(min, max) {
 
 // does allShipCoords have any of these coords?
 // return [] for true, original parameters otherwise
-const verifyGeneratedCoords = (generatedCoordinates) => {
+const verifyGeneratedCoords = (generatedCoordinates, shipName) => {
   for (let coords of generatedCoordinates) {
-    if (
-      allShipCoords.some(([xc, yc]) => xc === coords[0] && yc === coords[1])
-    ) {
+    const str = JSON.stringify(coords);
+    if (allShipCoords.has(str)) {
+      // console.log("found", coords, shipName);
       return [];
     }
-    return generatedCoordinates;
   }
+  const sizeBefore = allShipCoords.size;
+
+  generatedCoordinates.forEach((s) => {
+    allShipCoords.add(JSON.stringify(s));
+  });
+  if (sizeBefore + generatedCoordinates.length !== allShipCoords.size)
+    console.log("fishy", shipName);
+  shipSizeMap[shipName].coords.push(generatedCoordinates);
+  return generatedCoordinates;
 };
 
 // generate coordinates
@@ -47,7 +54,6 @@ const verifyGeneratedCoords = (generatedCoordinates) => {
 const cols = document.getElementById("grid").children;
 Object.keys(shipSizeMap).forEach((shipName) => {
   const { size, num } = shipSizeMap[shipName];
-  shipSizeMap[shipName].coords = [];
   let count = num;
   let arr = [];
 
@@ -56,38 +62,33 @@ Object.keys(shipSizeMap).forEach((shipName) => {
     const yInt = randomIntFromInterval(0, 9);
     if (xInt - size > 0) {
       arr = new Array(size).fill(0).map((_, i) => [xInt - i, yInt]);
-      if (verifyGeneratedCoords(arr).length) {
-        allShipCoords.push(...arr);
-        shipSizeMap[shipName].coords.push(arr);
-        break;
+      if (!!verifyGeneratedCoords(arr, shipName).length) {
+        count--;
+        continue;
       }
     }
     if (yInt - size > 0) {
       arr = new Array(size).fill(0).map((_, i) => [xInt, yInt - i]);
-      if (verifyGeneratedCoords(arr).length) {
-        allShipCoords.push(...arr);
-        shipSizeMap[shipName].coords.push(arr);
-        break;
+      if (!!verifyGeneratedCoords(arr, shipName).length) {
+        count--;
+        continue;
       }
     }
     if (yInt + size < 10) {
       arr = new Array(size).fill(0).map((_, i) => [xInt, yInt + i]);
-      if (verifyGeneratedCoords(arr).length) {
-        allShipCoords.push(...arr);
-        shipSizeMap[shipName].coords.push(arr);
-        break;
+      if (!!verifyGeneratedCoords(arr, shipName).length) {
+        count--;
+        continue;
       }
     }
     if (xInt + size < 10) {
       arr = new Array(size).fill(0).map((_, i) => [xInt + i, yInt]);
-      if (verifyGeneratedCoords(arr).length) {
-        allShipCoords.push(...arr);
-        shipSizeMap[shipName].coords.push(arr);
-        break;
+      if (!!verifyGeneratedCoords(arr, shipName).length) {
+        count--;
+        continue;
       }
     }
     if (arr.length === 0) console.log(`${shipName} did not get generated`);
-    count--;
   }
 });
 
@@ -95,30 +96,35 @@ Array.from(document.querySelectorAll(".cell")).forEach((c) => {
   c.addEventListener("click", (e) => {
     const parents = Array.from(container.children);
     const siblings = Array.from(e.target.parentElement.children);
-    // [x, y]
-    const xc = siblings.indexOf(e.target);
-    const yc = parents.indexOf(e.target.parentElement);
+    const x = siblings.indexOf(e.target);
+    const y = parents.indexOf(e.target.parentElement);
 
-    const index = allShipCoords.findIndex(([x, y]) => x === xc && y === yc);
-    // remove
-    if (index > -1) {
-      allShipCoords.splice(index, 1);
-      console.log("hit");
+    const str = JSON.stringify([x, y]);
+    if (allShipCoords.has(str)) {
+      allShipCoords.delete(str);
+      if (allShipCoords.size === 0)
+        document.getElementById("finish").classList.remove("hide");
+      else document.getElementById("hit").classList.remove("hide");
+    } else {
+      document.getElementById("hit").classList.add("hide");
     }
-    if (allShipCoords.length === 0)
-      document.getElementById("finish").classList.remove("hide");
   });
 });
 
-// draw them out on container for testing purposes
-Object.keys(shipSizeMap).forEach((k) => {
-  shipSizeMap[k].coords.forEach((coords) => {
+// draw boats for testing purposes
+Object.keys(shipSizeMap).forEach((shipName) => {
+  shipSizeMap[shipName].coords.forEach((coords) => {
     coords.forEach(([x, y]) => {
-      if (k === "Carrier") cols[y].children[x].style.background = "red";
-      if (k === "Battleship") cols[y].children[x].style.background = "yellow";
-      if (k === "Destroyer") cols[y].children[x].style.background = "blue";
-      if (k === "Submarine") cols[y].children[x].style.background = "green";
-      if (k === "Patrol Boat") cols[y].children[x].style.background = "purple";
+      if (shipName === "Carrier") cols[y].children[x].style.background = "pink";
+      if (shipName === "Battleship")
+        cols[y].children[x].style.background = "yellow";
+      if (shipName === "Destroyer")
+        cols[y].children[x].style.background = "lightblue";
+      if (shipName === "Submarine")
+        cols[y].children[x].style.background = "lightgreen";
+      if (shipName === "Patrol Boat")
+        cols[y].children[x].style.background = "orange";
+      cols[y].children[x].textContent = `${shipName}`;
     });
   });
 });
