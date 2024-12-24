2343b98d70b85fcf9ad1075c1cd2b65cd4b4d1d8
Anny He <annyannyhehe@gmail.com>
2024-12-23 21:50:14 -0800

    interim commit

--- solo-mafia/script.js
+++ solo-mafia/script.js
@@ -1,15 +1,14 @@
-let numAllCount = 5;
-let mafiaFolkCount = 2;
-let townFolkCount = numAllCount - mafiaFolkCount;
 // TODO: self can be townsfolk OR mafia
 const allFolk = [0, 1, 2, 3, 4];
 shuffleArray(allFolk);
 mafiaFolk = [allFolk.pop(), allFolk.pop()].sort((a, b) => a - b);
 const townsFolk = allFolk.sort((a, b) => a - b);
-
+let playerNumber = townsFolk[0];
 // TODO: self is a number, cannot pick self to be killed
 let isEnded = false;
 let isDaytime = true;
+const roleElems = document.getElementsByClassName("role");
+roleElems[0].textContent += " " + playerNumber;
 const totalElems = document.getElementsByClassName("total");
 const dayTimeElems = document.getElementsByClassName("dayTime");
 const killElems = document.getElementsByClassName("kill");
@@ -20,17 +19,14 @@ const showEnd = () => {
   const endElems = document.getElementsByClassName("end");
   endElems[0].classList.remove("hide");
   endElems[0].textContent =
+    // JSON.stringify(townsFolk) + JSON.stringify(mafiaFolk);
     townsFolk.length === 0 ? "Mafia won" : "Townsfolk won";
 
   const playElems = document.getElementsByClassName("play");
   playElems[0].classList.add("hide");
 };
 nextElems[0].addEventListener("click", () => {
-  if (
-    mafiaFolkCount === numAllCount ||
-    townFolkCount === numAllCount ||
-    (mafiaFolkCount === 0 && townFolkCount === 0)
-  ) {
+  if (mafiaFolk.length === 0 || townsFolk.length === 0) {
     showEnd();
   }
   let killIntElems = document.getElementsByClassName("killInt");
@@ -42,9 +38,7 @@ nextElems[0].addEventListener("click", () => {
       townFolkCount = townsFolk.length;
     } else {
       mafiaFolk.splice(mafiaFolk.indexOf(int), 1);
-      mafiaFolkCount = mafiaFolk.length;
     }
-    numAllCount -= 1;
 
     killIntElems[0].value = "";
   }
@@ -60,18 +54,16 @@ nextElems[0].addEventListener("click", () => {
   } else {
     // nighttime: randomly kill a townsperson
     killElems[0].classList.add("hide");
+    const killedInt = townsFolk.pop();
+    console.log("killed", killedInt);
+    townFolkCount = townsFolk.length;
+    killedElems[0].textContent += killedInt + ", ";
+    killedElems[0].classList.remove("hide");
     if (!townsFolk.length) {
       // game over
       showEnd();
     }
-    const killedInt = townsFolk.pop();
-    console.log("killed", killedInt);
-    townFolkCount = townsFolk.length;
-    killedElems[0].textContent += killedInt + ",";
-    killedElems[0].classList.remove("hide");
   }
-  // decrement the numAllCount
-  numAllCount = townFolkCount + mafiaFolkCount;
 
   totalElems[0].textContent = JSON.stringify([...townsFolk, ...mafiaFolk]);
 });
