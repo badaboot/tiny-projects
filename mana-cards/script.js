2e3be6adaaef042ab7172667d1c69bcb6501f60b
Anny He <annyannyhehe@gmail.com>
2025-01-12 15:11:05 -0800

    add card logic

--- mana-cards/script.js
+++ mana-cards/script.js
@@ -1,3 +1,14 @@
+const MAX_POWER = 8;
+const powerCountElem = document.getElementById("powerCount");
+powerCountElem.textContent = MAX_POWER;
+
+const cardsElem = document.getElementById("holder");
+// TODO: replace with particles
+const powerUpElem = document.getElementById("power-up");
+powerUpElem.addEventListener("click", () => {
+  powerCountElem.textContent = parseInt(powerCountElem.textContent, 10) + 1;
+});
+
 function dragStart(ev) {
   ev.dataTransfer.effectAllowed = "move";
   ev.dataTransfer.setData("Text", ev.target.getAttribute("id"));
@@ -17,7 +28,45 @@ function dragOver(ev) {
 // function defined for when drop element on target
 function dragDrop(ev) {
   var data = ev.dataTransfer.getData("Text");
-  ev.target.appendChild(document.getElementById(data));
+  const power = document.getElementById(data).textContent;
+  const res = parseInt(powerCountElem.textContent - power, 10);
+  console.log(power);
+  if (res >= 0) {
+    powerCountElem.textContent = res;
+    cardsElem.removeChild(document.getElementById(data));
+  }
+  if (cardsElem.children.length === 0) {
+    getCards();
+  }
   ev.stopPropagation();
   return false;
 }
+
+// cards logic
+// TODO: randomize cards
+const getCards = () => {
+  const cards = [
+    { color: "blue", power: 1 },
+    { color: "purple", power: 1 },
+    { color: "purple", power: 2 },
+    { color: "purple", power: 3 },
+    { color: "green", power: 1 },
+    { color: "red", power: 1 },
+  ];
+
+  // TODO: need the id
+  cards.forEach(({ color, power }, index) => {
+    /*
+    <div
+          class="drag"
+          id="boxA"
+          draggable="true"
+          ondragstart="return dragStart(event)"
+        ></div>
+    */
+    cardsElem.children[index].textContent = power;
+    cardsElem.children[index].style.background = color;
+  });
+};
+
+getCards();
