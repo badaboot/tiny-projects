7522c7ab78dacb578a04c65952b9eab8c676601b
Anny He <annyannyhehe@gmail.com>
2025-05-16 20:59:29 -0700

    add credits

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,9 +1,26 @@
 const board = document.querySelector('.board');
 const clone = document.querySelector('.clone');
-const overlay = document.querySelector('.overlay');
-const reset = document.querySelector('.reset');
-// dino by default
-let tileOptions = ['erupt', 'ptero', 'tri', 'ahahah', 'egg', 'dino'];
+const playAgainBtn = document.querySelector('.play-again');
+const gameOverElement = document.querySelector('.gameover');
+const optionElems = document.querySelectorAll(`option`)
+let currentThemeId = 'fish'
+const tileOptionsMap = {
+    'fish': ['fish1', 'fish2', 'fish3', 'fish4', 'fish5', 'fish6'],
+    'eggs': ['blue', 'green', 'purple', 'red', 'white', 'yellow'],
+    'rpg': ['coin', 'crystal', 'mineral', 'paper', 'skull', 'wood'],
+    'flower': ['anthurium', 'daffodil', 'heather', 'pansies', 'protea', 'scarlet']
+}
+let tileOptions = tileOptionsMap['fish'];
+const seaMusic = new Audio("audio/sea.mp3");
+const flowerMusic = new Audio("audio/flower.mp3");
+const rpgMusic = new Audio("audio/rpg.mp3");
+const dragonMusic = new Audio("audio/dragon.mp3");
+const musicMap = {
+    'fish': seaMusic,
+    'eggs': dragonMusic,
+    'rpg': rpgMusic,
+    'flower': flowerMusic
+}
 const matchSound = new Audio("audio/winSound.wav");
 const menuClickSound = new Audio("audio/menuClick.wav");
 const winSound = new Audio("audio/yay.mp3");
@@ -15,12 +32,22 @@ const state = {
     matches: 0
 };
 
-reset.addEventListener('click', () => {
-    if (state.boardLocked) return;
+const showBoard = () => {
+    document.querySelector(`.some-page-wrapper`).classList.add('hide');
+}
+
+playAgainBtn.addEventListener('click', () => {
+    menuClickSound.play();
+    musicMap[currentThemeId].pause();
+    currentThemeId = document.querySelector(`select`).value
+    tileOptions = tileOptionsMap[currentThemeId]
     resetGame();
-});
+})
 
 function resetGame() {
+    showBoard();
+    musicMap[currentThemeId].play();
+    gameOverElement.classList.add('hide');
     state.boardLocked = true;
     state.selections = [];
     state.matches = 0;
@@ -30,7 +57,6 @@ function resetGame() {
         tile.remove();
     });
 
-    overlay.classList.add('hidden');
     createBoard();
 }
 
@@ -61,6 +87,10 @@ function buildTile(option, id) {
     tile.setAttribute('data-id', id);
     return tile;
 }
+// generates a random number between 0 and max exclusive
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
 function selectTile(selectedTile) {
     clickSound.play()
@@ -79,15 +109,17 @@ function selectTile(selectedTile) {
     if (state.selections.length === 2) {
         if (state.selections[0].tile === state.selections[1].tile) {
             window.setTimeout(() => {
-                state.selections[0].el.classList.add('matched');
-                state.selections[1].el.classList.add('matched');
+                state.selections[0].el.classList.add('matched', 'blink');
+                state.selections[1].el.classList.add('matched', 'blink');
 
                 state.boardLocked = false;
                 state.matches = state.matches + 1;
 
                 if (state.matches === tileOptions.length) {
                     window.setTimeout(() => {
-                        overlay.classList.remove('hidden');
+                        gameOverElement.classList.remove('hide');
+                        const index = getRandomInt(optionElems.length)
+                        optionElems[index].selected = true;
                         winSound.play();
                     }, 600);
                 }
@@ -117,17 +149,14 @@ function shuffleArray(array) {
 }
 
 for (let buttonParent of document.querySelectorAll('.column')) {
-    console.log(buttonParent);
     const buttonElem = buttonParent.querySelector('button');
     buttonElem.addEventListener('click', () => {
         menuClickSound.play();
-        console.log(buttonElem.dataset.id);
-        if (buttonElem.dataset.id === 'eggs') {
-            tileOptions = ['blue', 'green', 'purple', 'red', 'white', 'yellow']
-        }
-        board.classList.remove('half-opacity');
-        document.querySelector(`.some-page-wrapper`).classList.add('hide');
+        currentThemeId = buttonElem.dataset.id;
+        musicMap[currentThemeId].play();
+        musicMap[currentThemeId].loop = true;
+        tileOptions = tileOptionsMap[currentThemeId]
         createBoard();
-
+        showBoard()
     });
 }
