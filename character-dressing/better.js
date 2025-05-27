60fe667534250aef47aa259e4d7ff178fc819142
Anny He <annyannyhehe@gmail.com>
2025-05-26 20:06:12 -0700

    learned about absolute positioning

--- character-dressing/better.js
+++ character-dressing/better.js
@@ -1,4 +1,8 @@
 let beingShownId = null;
+const selectedOptions = {
+    hat: null,
+    top: null,
+}
 for (let button of document.getElementsByClassName("showButton")) {
     console.log(button.id)
     button.addEventListener("click", function () {
@@ -11,3 +15,40 @@ for (let button of document.getElementsByClassName("showButton")) {
 
     })
 }
+const parentOfGhost = document.getElementById('characterImage').parentElement
+const checkAncestorId = (element, id) => {
+    return element && (element.id === id || checkAncestorId(element.parentElement, id));
+}
+document.querySelectorAll('.option-group img').forEach(img => {
+    img.addEventListener('click', function () {
+        console.log(img.id, img.parentElement.parentElement);
+        if (checkAncestorId(img, 'hatOptions')) {
+            if (selectedOptions.hat) {
+                // change the src
+                const chosenHatNode = document.getElementById('chosenHat');
+                chosenHatNode.src = img.src;
+                chosenHatNode.classList.add('hatShow');
+            } else {
+                const clonedNode = img.cloneNode(true);
+                clonedNode.id = 'chosenHat'; // Clear the id to avoid duplicates
+                clonedNode.classList.add('hatShow');
+                parentOfGhost.appendChild(clonedNode);
+            }
+            selectedOptions.hat = img.id;
+        } else if (checkAncestorId(img, 'topOptions')) {
+            if (selectedOptions.top) {
+                // change the src
+                const chosenTopNode = document.getElementById('chosenTop');
+                chosenTopNode.src = img.src;
+                chosenTopNode.classList.add('topShow');
+            } else {
+                const clonedNode = img.cloneNode(true);
+                clonedNode.id = 'chosenTop'; // Clear the id to avoid duplicates
+                clonedNode.classList.add('topShow');
+                parentOfGhost.appendChild(clonedNode);
+            }
+            selectedOptions.top = img.id;
+            console.log(selectedOptions);
+        }
+    });
+});
